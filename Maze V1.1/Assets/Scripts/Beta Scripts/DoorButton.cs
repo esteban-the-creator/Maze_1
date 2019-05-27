d6ee7d725f84096f1ce7598b9f2f1ca98d748bd9
Esteban <estebangs98@gmail.com>
2019-05-26 22:33:00 -0500

    adds some interactables

---
+++ Maze V1.1/Assets/Scripts/Beta Scripts/DoorButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour
+{
+    public GameObject door;
+
+    private bool doorIsActive = true;
+
+    void OpenOrClose()
+    {
+        doorIsActive = !doorIsActive;
+        
+        if (doorIsActive)
+        {
+            door.SetActive(true);
+        }
+        else
+        {
+            door.SetActive(false);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        OpenOrClose();
+    }
+
+
+}
---
+++ Maze V1.1/Assets/Scripts/PlatformInterct.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformInteract : MonoBehaviour, IInteract
+{
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+}
