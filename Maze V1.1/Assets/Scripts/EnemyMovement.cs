b41180b8eb663729c4a8d2c23c9ac835f2c8dc2a
esteban-the-creator <estebangs98@gmail.com>
2019-05-27 20:44:09 -0500

    adds enemy behaviour
    patrullaje

---
+++ Maze V1.1/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Transform enemyTransform;
+    public List<Transform> waitPointTransforms;
+    [SerializeField]
+    private int actualWaitpoint = 0;
+    private float movementSpeed = 5f;
+
+    void Awake()
+    {
+        enemyTransform = transform;
+    }
+
+    public void Move()
+    {
+        Vector3 actualWaitPointDisplacement = waitPointTransforms[actualWaitpoint].position - enemyTransform.position;
+        float distanceToWaitPoint = actualWaitPointDisplacement.magnitude;
+
+        if (distanceToWaitPoint > 1)
+        {
+            Vector3 directionVector = actualWaitPointDisplacement.normalized;
+            enemyTransform.position += directionVector * movementSpeed * Time.deltaTime;
+        }
+
+        else
+        {
+            actualWaitpoint++;
+            if (actualWaitpoint >= waitPointTransforms.Count)
+                actualWaitpoint = 0;
+        }
+    }
+}
