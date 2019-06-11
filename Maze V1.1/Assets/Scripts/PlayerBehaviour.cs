54e0fc22d6c576b063061b4c125d790b47002e6e
esteban-the-creator <42298103+esteban-the-creator@users.noreply.github.com>
2019-06-10 20:39:35 -0500

    add things to the camera

---
+++ Maze V1.1/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponentInParent<IInteract>() != null)
+        {
+            other.GetComponentInParent<IInteract>().Interact();
+        }
+    }
+}
+
+public void Moverse
+{
+
+}
